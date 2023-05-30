876bea131386ee34cebf779f660bf990e48e039f
Rajesh18021998 <rabirajesh18@gmail.com>
2023-05-30 21:41:52 +0530

    Updated my code

---
+++ data_structure_01.py
@@ -0,0 +1,47 @@
+Python 3.10.10 (tags/v3.10.10:aad5f6a, Feb  7 2023, 17:20:36) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print("Hello world")
+Hello world
+>>> print("hau")
+hau
+>>> my_name = "Rajesh"
+>>> type(my_name)
+<class 'str'>
+>>> print(my_name)
+Rajesh
+>>> my_int = 5
+>>> type(my_int)
+<class 'int'>
+>>> print(my_int)
+5
+>>> my_float = 5.5
+>>> type(my_float)
+<class 'float'>
+>>> print(my_float)
+5.5
+>>> my_list = [5,14,123]
+>>> type(my_list)
+<class 'list'>
+>>> print(my_list)
+[5, 14, 123]
+>>> my_tuple = (123, 145, 123)
+>>> type(my_tuple)
+<class 'tuple'>
+>>> print(my_tuple)
+(123, 145, 123)
+>>> my_set = {123, 124, 167}
+type(
+    )
+Traceback (most recent call last):
+  File "<pyshell#18>", line 1, in <module>
+    type(
+TypeError: type() takes 1 or 3 arguments
+type(my_set)
+<class 'set'>
+print(my_set)
+{123, 124, 167}
+my_dic = {"Rajesh" : 754546446}
+type(my_dic)
+<class 'dict'>
+print(my_dic)
+{'Rajesh': 754546446}
