fae40afe844102721c28892ce0017238a6df5d20
Rajesh18021998 <rabirajesh18@gmail.com>
2023-05-30 21:59:05 +0530

    updated code as per TL advised

--- data_structure_01.py
+++ data_structure_01.py
@@ -1,6 +1,10 @@
 Python 3.10.10 (tags/v3.10.10:aad5f6a, Feb  7 2023, 17:20:36) [MSC v.1929 64 bit (AMD64)] on win32
 Type "help", "copyright", "credits" or "license()" for more information.
 >>> print("Hello world")
+
+"dai thambi is printing"
+"dai thambi is printing"
+
 Hello world
 >>> print("hau")
 hau
